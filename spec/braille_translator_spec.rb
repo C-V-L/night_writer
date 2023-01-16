7e754b0c347d2f1f15c2ce4ae7fd3b3c31d260b6
Conner Van Loan <cdvanloan@gmail.com>
2023-01-16 08:49:56 -0700

    Tests for #initialize brailletrans

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -1,11 +1,11 @@
-require './lib/'
+require './lib/braille_translator'
 
-RSpec.describe '' do
+RSpec.describe 'BrailleTranslator' do
 
+  let(:translator) { BrailleTranslator.new}
   describe '#initialize' do
     it 'exists' do
-      
-      expect().to be_a()
+      expect(translator).to be_a(BrailleTranslator)
     end
   end
 end
