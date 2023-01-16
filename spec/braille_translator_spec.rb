934f03122dd247a559b15eaf4038965faa95a79c
Conner Van Loan <cdvanloan@gmail.com>
2023-01-16 08:52:47 -0700

    Adds test for empty eng_to_braille library

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -1,11 +1,15 @@
 require './lib/braille_translator'
 
 RSpec.describe 'BrailleTranslator' do
-
   let(:translator) { BrailleTranslator.new}
+
   describe '#initialize' do
     it 'exists' do
       expect(translator).to be_a(BrailleTranslator)
+    end
+
+    it 'has a library of english to braille' do
+      expect(translator.eng_to_braille).to eq({})
     end
   end
 end
