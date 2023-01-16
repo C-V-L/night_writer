da82fba6de74fba44b7ab248dbb50bd5513beb56
Conner Van Loan <cdvanloan@gmail.com>
2023-01-16 09:17:51 -0700

    adds test for key1

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe 'BrailleTranslator' do
       expect(translator).to be_a(BrailleTranslator)
     end
 
-    it 'has a library of english to braille' do
+    xit 'has a library of english to braille' do
       expect(translator.eng_to_braille).to eq({})
+    end
+  end
+
+  describe 'braille keys' do
+    describe 'key1' do
+      it 'returns a string of braille' do
+        expect(translator.key1).to eq('00')
+      end
     end
   end
 end
