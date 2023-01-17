2fb911c943923cfd41f8355e7568cd022a2e8517
Conner Van Loan <cdvanloan@gmail.com>
2023-01-17 12:22:16 -0700

    Adds test for #write_file

--- spec/file_handler_spec.rb
+++ spec/file_handler_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe 'FileHandler' do
       expect(FileHandler.read_file).to be_a(File)
     end
   end
+
+  describe '#write_file' do
+    it 'can write a file' do
+      allow(File).to receive(:new).and_return(File.open('./fixtures/hello_world_fixture.txt', 'r'))
+      expect(FileHandler.write_file).to be_a(File)
+    end
+  end
 end
