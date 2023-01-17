ccf19b0217f1ac72fcbf5c461b61b9bc0cbb9423
Conner Van Loan <cdvanloan@gmail.com>
2023-01-17 12:20:51 -0700

    Adds #read_file method and passes test

---
+++ lib/file_handler.rb
@@ -0,0 +1,30 @@
+class FileHandler
+
+
+  def self.read_file
+    File.open(ARGV[0], 'r')
+  end
+
+  # def self.write_file
+  #   File.new(ARGV[1], 'w+')
+  # end
+
+  # def out_put_char_count
+    
+  # end
+end
+
+# require_relative 'translator'
+
+# @user_input = ARGV 
+
+# translator = Translator.new
+
+# read_message = File.read(ARGV[0])
+# new_file = File.new(ARGV[1], 'w+')
+
+# translation = translator.formatted_braille_conversion(read_message)
+# new_file.write(translation)
+# p "Created #{ARGV[1]} containing #{read_message.length} characters"
+
+# new_file.close
