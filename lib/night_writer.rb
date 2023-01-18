625df14264681d26632701805ac083cbc59b00fc
Conner Van Loan <cdvanloan@gmail.com>
2023-01-17 17:19:56 -0700

    Edits night_writer to call correct method inside translate

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,6 +6,7 @@ translator = Translator.new
 read_file = FileHandler.read_file
 write_file = FileHandler.write_file
 
-translation = translator.formatted_braille_conversion(FileHandler.read_file.read)
+translation = translator.formatted_braille_conversion(read_file.read)
 write_file.write(translation)
+
 puts " Created #{ARGV[1]} containing #{FileHandler.count_english_characters} characters."
